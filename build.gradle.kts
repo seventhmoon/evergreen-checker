6b15019a023f26228cf0baeb0e4b1a052987e6ab
Manas Tungare <manas@google.com>
2021-05-26 06:17:36 -0700

    Update `kotlin_version` from `1.4.21` to `1.5.0`

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-  extra["kotlin_version"] = "1.4.21"
+  extra["kotlin_version"] = "1.5.0"
 
   repositories {
     google()
